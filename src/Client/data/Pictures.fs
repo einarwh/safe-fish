5d01af3d84b58b877dd92964a347d193c1425195
Einar W. Høst <einarwh@gmail.com>
2017-11-10 23:34:37 +0100

    turns times

--- src/Client/data/Pictures.fs
+++ src/Client/data/Pictures.fs
@@ -11,3 +11,9 @@ type Picture = Box -> Rendering
 let blank : Picture = fun _ -> []
 
 let turn p = turnBox >> p
+
+let rec times n fn = 
+  if n = 0 then id
+  else fn >> times (n - 1) fn
+
+let turns n = times n turn
